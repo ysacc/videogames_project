8fa3cd7433eb8deea433638e845b6c706fe66894
Ysacc Roncal <samironcal@gmail.com>
2023-02-24 20:38:48 -0500

    modelos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,35 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey:  true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    releaseDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    rating:{
+      type: DataTypes.FLOAT,
+      allowNull: false
     },
   });
 };
