08b8e4d19933184f2a7052bd61d3fa2551dc84a5
Ysacc Roncal <samironcal@gmail.com>
2023-02-28 11:28:31 -0500

    back

--- api/src/controllers/getGameByName.js
+++ api/src/controllers/getGameByName.js
@@ -2,24 +2,20 @@ const axios = require("axios");
 const {API_KEY} = process.env;
 
 const getGameByName = async(req,res)=>{
+    const {game} = req.query;
     try {
         // Obtener los videojuegos que coincidan con el nombre especificado
-        const gameByName = await Videogame.findAll({
-            where: {
-            name: {
-                [Op.iLike]: `%${req.query.name}%`
-            }
-            },
-            limit: 15,
-            include: Genres
-        });
-        if (vgameByName.length > 0) {
-            // res.json(videogames);
-            return gameByName;
-        } else {
-            res.status(404).send('Videojuegos no encontrados');
+        const response = await axios (`https://api.rawg.io/api/games?search=${game}?api_key=${API_KEY}`);
+        const data = response.data;
+        if (!data){
+            res.status(404).send("No se encontraron resultados");
         }
-    } catch (error) {
+        else{
+            const games = data.map(game=>{})
+            res.status(200).json(games)
+        } 
+    }
+    catch (error) {
         // console.error(error);
         // res.status(400).send('Error al buscar los videojuegos');
         res.status(404).json({error:error.message})
